117e3a3a9a13025b14b89d0143b8d773129e957c
Anthony Rees <anthony_rees01@hotmail.com>
2019-05-14 11:06:35 +1000

    update

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,13 +5,6 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
-
 # This is an example test, replace it with your own test.
 describe port(8080) do
   it { should be_listening }
@@ -19,4 +12,21 @@ end
 
 describe command('curl localhost:8080') do
   its('stdout') { should match ('Hello, world')}
+end
+
+control 'Linux VERSION' do
+  impact 0.8
+  title 'This test checks for a minimum Linux version'
+
+  describe os.family do
+    it { should eq 'centos' }
+  end
+
+  describe os.name do
+    it { should eq 'centos6' }
+  end
+
+  describe os.release do
+    it { should > '6' }
+  end
 end
